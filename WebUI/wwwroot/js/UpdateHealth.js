13a5142edb29a272f033e990560dbeabfb3f04a2
rchumbert <rchumbert@ksu.edu>
2022-11-12 21:19:20 -0600

    UpdateHealth done

--- WebUI/wwwroot/js/UpdateHealth.js
+++ WebUI/wwwroot/js/UpdateHealth.js
@@ -1,4 +1,4 @@
-﻿// ********** MultipleChoice.js *************
+﻿// ********** UpdateHealth.js *************
 var hp = 2;
 function updateHeroHealth(currentHP) {
     const health = document.querySelectorAll(".heroHP");
@@ -11,10 +11,22 @@ function resetHealth() {
     reset.forEach(element => element.src = "Imgs/HealthFull.png")
 }
 
-function updateMonHealth(currentHP) {
+/*function updateMonHealth(currentHP) {
     const health = document.querySelectorAll(".heroHP");
     health[currentHP].src = "Imgs/HealthEmpty.png";
-}
+}*/
 function test() {
     updateHeroHealth(hp);
+}
+
+function testRestore() {
+    resetHealth();
+}
+
+const click = document.querySelectorAll(".answer");
+click[0].onclick = function () {
+    updateHeroHealth(hp);
+}
+click[1].onclick = function () {
+    resetHealth();
 }
